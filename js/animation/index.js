e8c75b1a85267135e61bd2b42506edae98b483e1
Suryansh Singh <supremesofficialyt@gmail.com>
2024-06-30 20:09:20 +0530

    Delete js/animation directory

--- js/animation/index.js
+++
@@ -1,4 +0,0 @@
-export { default as animationEnter } from "./animationEnter";
-export { default as animationLeave } from "./animationLeave";
-export { default as pageTransition } from "./pageTransition";
-export { default as aboutAnimationEnter } from "./aboutAnimationEnter";
